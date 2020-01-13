5d2add5db6a0513521997d2411f8505e50f00c05
Nick Steinhauser <Nick.Steinhauser@ibm.com>
2020-01-13 10:48:17 -0600

    manual with meta urls

--- manual/test-combos.sh
+++ manual/test-combos.sh
@@ -5,152 +5,152 @@ set -e
 echo "========================================================================="
 echo "Testing spring knative no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-spring-knative-no-svc.json https://github.com/IBM/spring-microservice.git
+./test-combo.sh test-genv2-app-spring-knative-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing spring knative cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-spring-knative-cloudant.json https://github.com/IBM/spring-microservice.git
+./test-combo.sh test-genv2-app-spring-knative-cloudant.json meta
 
 echo "========================================================================="
 echo "Testing spring knative appid"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-spring-knative-appid.json https://github.com/IBM/spring-microservice.git
+./test-combo.sh test-genv2-app-spring-knative-appid.json meta
 
 echo "========================================================================="
 echo "Testing spring knative cos"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-spring-knative-cos.json https://github.com/IBM/spring-microservice.git
+./test-combo.sh test-genv2-app-spring-knative-cos.json meta
 
 
 
 echo "========================================================================="
 echo "Testing liberty knative no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-liberty-knative-no-svc.json https://github.com/IBM/java-liberty-microservice.git
+./test-combo.sh test-genv2-app-liberty-knative-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing liberty knative cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-liberty-knative-cloudant.json https://github.com/IBM/java-liberty-microservice.git
+./test-combo.sh test-genv2-app-liberty-knative-cloudant.json meta
 
 
 
 echo "========================================================================="
 echo "Testing spring helm no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-spring-helm-no-svc.json https://github.com/IBM/spring-microservice.git
+./test-combo.sh test-genv2-app-spring-helm-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing spring helm cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-spring-helm-cloudant.json https://github.com/IBM/spring-microservice.git
+./test-combo.sh test-genv2-app-spring-helm-cloudant.json meta
 
 echo "========================================================================="
 echo "Testing spring helm cos"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-spring-helm-cos.json https://github.com/IBM/spring-microservice.git
+./test-combo.sh test-genv2-app-spring-helm-cos.json meta
 
 echo "========================================================================="
 echo "Testing spring helm appid"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-spring-helm-appid.json https://github.com/IBM/spring-microservice.git
+./test-combo.sh test-genv2-app-spring-helm-appid.json meta
 
 
 
 echo "========================================================================="
 echo "Testing go knative no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-go-knative-no-svc.json https://github.com/IBM/go-microservice.git
+./test-combo.sh test-genv2-app-go-knative-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing go knative cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-go-knative-cloudant.json https://github.com/IBM/go-microservice.git
+./test-combo.sh test-genv2-app-go-knative-cloudant.json meta
 
 echo "========================================================================="
 echo "Testing go knative appid"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-go-knative-appid.json https://github.com/IBM/go-microservice.git
+./test-combo.sh test-genv2-app-go-knative-appid.json meta
 
 
 
 echo "========================================================================="
 echo "Testing node express knative no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-express-knative-no-svc.json https://github.com/IBM/nodejs-microservice.git
+./test-combo.sh test-genv2-app-express-knative-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing node express knative cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-express-knative-cloudant.json https://github.com/IBM/nodejs-microservice.git
+./test-combo.sh test-genv2-app-express-knative-cloudant.json meta
 
 echo "========================================================================="
 echo "Testing node express knative appid"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-express-knative-appid.json https://github.com/IBM/nodejs-microservice.git
+./test-combo.sh test-genv2-app-express-knative-appid.json meta
 
 
 echo "========================================================================="
 echo "Testing swift knative no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-swift-knative-no-svc.json https://github.com/IBM/swift-web-app.git
+./test-combo.sh test-genv2-app-swift-knative-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing swift knative cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-swift-knative-cloudant.json https://github.com/IBM/swift-web-app.git
+./test-combo.sh test-genv2-app-swift-knative-cloudant.json meta
 
 echo "========================================================================="
 echo "Testing swift knative appid"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-swift-knative-appid.json https://github.com/IBM/swift-web-app.git
+./test-combo.sh test-genv2-app-swift-knative-appid.json meta
 
 
 
 echo "========================================================================="
 echo "Testing flask knative no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-knative-no-svc.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-knative-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing flask knative cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-knative-cloudant.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-knative-cloudant.json meta
 
 echo "========================================================================="
 echo "Testing flask knative appid"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-knative-appid.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-knative-appid.json meta
 
 
 echo "========================================================================="
 echo "Testing flask helm no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-helm-no-svc.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-helm-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing flask helm cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-helm-cloudant.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-helm-cloudant.json meta
 
 echo "========================================================================="
 echo "Testing flask helm appid"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-helm-appid.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-helm-appid.json meta
 
 
 echo "========================================================================="
 echo "Testing flask cf no svc"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-cf-no-svc.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-cf-no-svc.json meta
 
 echo "========================================================================="
 echo "Testing flask cf cloudant"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-cf-cloudant.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-cf-cloudant.json meta
 
 echo "========================================================================="
 echo "Testing flask cf appid"
 echo "========================================================================="
-./test-combo.sh test-genv2-app-python-cf-appid.json https://github.com/IBM/flask-web-app.git
+./test-combo.sh test-genv2-app-python-cf-appid.json meta
