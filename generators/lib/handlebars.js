584b423fcfd579b0fa21da09a0cb4437fd39ba60
Gabriel Valencia <gvalenc@us.ibm.com>
2020-01-22 13:22:38 -0600

    chore: service util cleanup
    Co-authored-by: Nick Steinhauser <nicksteinhauser@gmail.com>

--- generators/lib/handlebars.js
+++ generators/lib/handlebars.js
@@ -1,5 +1,5 @@
 /*
- © Copyright IBM Corp. 2017, 2018
+ © Copyright IBM Corp. 2019, 2020
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
@@ -102,11 +102,5 @@ Handlebars.registerHelper('exists', function(context, handler) {
 		return undefined
 	}
 });
-/*
-Handlebars.registerHelper('tag', function(context) {
-	return "{{" + context + "}}";
-});
-*/
-
 
 module.exports = Handlebars;
